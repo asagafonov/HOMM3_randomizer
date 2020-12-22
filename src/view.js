de19f9c8737428c9f4fe7c6f4300fd51480dd5e3
asagafonov <a.s.agafonov@yandex.ru>
2020-12-22 21:47:52 +0300

    add some view improvements

--- src/view.js
+++ src/view.js
@@ -20,32 +20,35 @@ const fillPage = (elements) => {
 
 const initView = (state, elements) => {
   const watchedState = onChange(state, (path, value) => {
+    const { cards, resultWindow } = elements;
     if (path.match(/towns/)) {
-      const { cards } = elements;
       cards.forEach((card) => {
         const { id } = card;
         const btn = card.querySelector('a');
+        const txt = card.querySelector('h6');
         if (state.towns.unbanned.includes(id)) {
           card.classList.remove('border-danger');
           btn.classList.remove('btn-danger');
           btn.classList.add('btn-secondary');
           btn.textContent = i18next.t('buttons.ban');
+          txt.classList.remove('text-danger');
         }
         if (state.towns.banned.includes(id)) {
           card.classList.add('border-danger');
           btn.classList.remove('btn-secondary');
           btn.classList.add('btn-danger');
           btn.textContent = i18next.t('buttons.unban');
+          txt.classList.add('text-danger');
         }
       });
     }
     if (path.match(/one/) && value === 'active') {
       const randomTown = generateRandomTown(state.towns.unbanned);
-      elements.resultWindow.textContent = randomTown;
+      resultWindow.textContent = randomTown;
     }
     if (path.match(/two/) && value === 'active') {
       const randomTowns = generateRandomTowns(state.towns.unbanned, numberOfAdversaries);
-      elements.resultWindow.textContent = randomTowns;
+      resultWindow.textContent = randomTowns;
     }
   });
   return watchedState;
