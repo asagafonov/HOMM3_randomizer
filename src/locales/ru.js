13b62b934a3d449bd01719c761e9310af75a2ea0
asagafonov <a.s.agafonov@yandex.ru>
2020-12-22 18:03:14 +0300

    initial russian language setup

---
+++ src/locales/ru.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    towns: {
+      castle: 'Замок',
+      rampart: 'Оплот',
+      tower: 'Башня',
+      inferno: 'Инферно',
+      necropolis: 'Некрополис',
+      dungeon: 'Подземелье',
+      citadel: 'Цитадель',
+      fortress: 'Крепость',
+      conflux: 'Сопряжение',
+      cove: 'Причал',
+    },
+    buttons: {
+      ban: 'Забанить',
+      unban: 'Разбанить',
+    }
+  },
+};
