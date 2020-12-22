79a2df969ef3c7eb2b008d165aa2d4eb8a9c4326
asagafonov <a.s.agafonov@yandex.ru>
2020-12-22 18:03:48 +0300

    edit locales

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,5 +1,20 @@
 export default {
   translation: {
-    key: 'value',
+    towns: {
+      castle: 'Castle',
+      rampart: 'Rampart',
+      tower: 'Tower',
+      inferno: 'Inferno',
+      necropolis: 'Necropolis',
+      dungeon: 'Dungeon',
+      citadel: 'Citadel',
+      fortress: 'Fortress',
+      conflux: 'Conflux',
+      cove: 'Cove',
+    },
+    buttons: {
+      ban: 'Ban',
+      unban: 'Unban',
+    }
   },
 };
